143123c0185c95f1dd381cb10b35934a9e4d5122
İbrahim Koç <112804288+iibrahimkoc@users.noreply.github.com>
2024-06-01 10:19:15 +0300

    Update 7.Reverse-integer.js

--- 7.Reverse-integer.js
+++ 7.Reverse-integer.js
@@ -3,11 +3,9 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
     let a = Math.sign(x);
     let num = Math.abs(x).toString().split('').reverse().join('') * a;
     if(x < Math.pow(-2,31) || x > Math.pow(2,31)-1)
         return 0;
     return num;
 };
-console.log(reverse(-134))
