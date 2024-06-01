93cd2aa8cf3b5717f0fb7d6457106be2d83a0180
İbrahim Koç <112804288+iibrahimkoc@users.noreply.github.com>
2024-06-01 10:18:50 +0300

    Add files via upload

---
+++ 7.Reverse-integer.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    
+    let a = Math.sign(x);
+    let num = Math.abs(x).toString().split('').reverse().join('') * a;
+    if(x < Math.pow(-2,31) || x > Math.pow(2,31)-1)
+        return 0;
+    return num;
+};
+console.log(reverse(-134))
